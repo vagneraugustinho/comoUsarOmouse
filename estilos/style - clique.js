c3816b828e6906bfae281c701e99eb5f08018cec
vagnereu <vagneraugustinho@gmail.com>
2018-07-12 10:46:30 -0300

    enviando o mouse

---
+++ estilos/style - clique.js
@@ -0,0 +1,567 @@
+$(document).ready(function(){
+    
+$("#avancar").hide();
+var count = 1;        
+
+$("#reiniciar").click(function(){
+    window.location.reload(true);
+});
+
+
+
+$("#mensagens").hide();
+$("#02").hide();
+$("#03").hide();
+$("#04").hide();
+$("#05").hide();
+$("#06").hide();
+$("#07").hide();
+$("#08").hide();
+$("#09").hide();
+$("#10").hide();
+$("#11").hide();
+$("#12").hide();
+$("#13").hide();
+$("#14").hide();
+$("#15").hide();
+$("#16").hide();
+$("#17").hide();
+$("#18").hide();
+$("#19").hide();
+$("#20").hide();
+$("#21").hide();
+$("#22").hide();
+$("#23").hide();
+$("#24").hide();
+$("#25").hide();
+$("#26").hide();
+$("#27").hide();
+$("#28").hide();
+$("#29").hide();
+$("#30").hide();
+$("#31").hide();
+$("#32").hide();
+$("#33").hide();
+$("#34").hide();
+$("#35").hide();
+$("#36").hide();
+$("#37").hide();
+$("#38").hide();
+$("#39").hide();
+$("#40").hide();
+$("#41").hide();
+$("#42").hide();
+$("#43").hide();
+$("#44").hide();
+$("#45").hide();
+$("#46").hide();
+$("#47").hide();
+$("#48").hide();
+$("#49").hide();
+$("#50").hide();
+$("#51").hide();
+$("#52").hide();
+$("#53").hide();
+$("#54").hide();
+$("#55").hide();
+$("#56").hide();
+$("#57").hide();
+$("#58").hide();
+$("#59").hide();
+$("#60").hide();
+
+//01-60
+$("#01").click(function(){
+    $("#addCount").val(count + ' Ponto');
+    count++;
+    $("#60").show();
+    $("#01").hide();
+});
+
+//60-01-02
+$("#60").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#60").hide();
+    $("#02").show();
+});
+
+//02-59
+$("#02").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#02").hide();
+    $("#59").show();
+});
+
+//59-23
+$("#59").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#59").hide();
+    $("#23").show();
+});
+
+//23-10
+$("#23").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#23").hide();
+    $("#10").show();
+});
+
+//10-51
+ $("#10").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#10").hide();
+    $("#51").show();
+});
+
+
+//51-25
+ $("#51").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#51").hide();
+    $("#25").show();
+});
+
+//25-41
+$("#25").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#25").hide();
+    $("#41").show();
+});
+
+//41-30
+$("#41").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#41").hide();
+    $("#30").show();
+});
+
+
+//30-52
+$("#30").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#30").hide();
+    $("#52").show();
+});
+
+//52-08
+$("#52").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#52").hide();
+    $("#08").show();
+});
+
+ //08-11
+ $("#08").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#08").hide();
+    $("#11").show();
+});
+
+
+ //11-56
+$("#11").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#11").hide();
+    $("#56").show();
+});
+
+
+//56-21
+$("#56").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#56").hide();
+    $("#21").show();
+});
+
+
+ //21-54
+$("#21").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#21").hide();
+    $("#54").show();
+});
+
+
+ //54-20
+$("#54").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#54").hide();
+    $("#20").show();
+});
+
+ //20-03
+$("#20").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#20").hide();
+    $("#03").show();
+});
+
+
+ //03-55
+$("#03").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#03").hide();
+    $("#55").show();
+});
+
+ //55-09
+$("#55").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#55").hide();
+    $("#09").show();
+});
+
+ //09-57
+$("#09").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#09").hide();
+    $("#57").show();
+});
+
+ //57-12
+$("#57").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#57").hide();
+    $("#12").show();
+});
+
+ //12-58
+$("#12").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#12").hide();
+    $("#58").show();
+});
+
+ //58-05
+$("#58").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#58").hide();
+    $("#05").show();
+});
+
+ //05-39
+$("#05").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#05").hide();
+    $("#39").show();
+});
+
+ //39-34
+$("#39").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#39").hide();
+    $("#34").show();
+});
+
+ //34-40
+$("#34").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#34").hide();
+    $("#40").show();
+});
+
+ //40-06
+$("#40").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#40").hide();
+    $("#06").show();
+});
+
+
+ //06-46
+$("#06").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#06").hide();
+    $("#46").show();
+});
+
+//46-04
+$("#46").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#46").hide();
+    $("#04").show();
+});
+
+//04-50
+$("#04").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#04").hide();
+    $("#50").show();
+});
+
+//50-45
+$("#50").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#50").hide();
+    $("#45").show();
+});
+
+//45-13
+$("#45").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#45").hide();
+    $("#13").show();
+});
+
+//13-49
+$("#13").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#13").hide();
+    $("#49").show();
+});
+
+//49-43
+$("#49").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#49").hide();
+    $("#43").show();
+});
+
+//49-27
+$("#43").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#43").hide();
+    $("#27").show();
+});
+
+//27-53
+$("#27").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#27").hide();
+    $("#53").show();
+});
+
+//53-38
+$("#53").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#53").hide();
+    $("#38").show();
+});
+
+//38-14
+$("#38").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#38").hide();
+    $("#14").show();
+});
+
+//14-19
+$("#14").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#14").hide();
+    $("#19").show();
+});
+
+//19-44
+$("#19").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#19").hide();
+    $("#44").show();
+});
+
+//44-29
+$("#44").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#44").hide();
+    $("#29").show();
+});
+
+//29-47
+$("#29").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#29").hide();
+    $("#47").show();
+});
+
+//47-31
+$("#47").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#47").hide();
+    $("#31").show();
+});
+
+//31-18
+$("#31").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#31").hide();
+    $("#18").show();
+});
+
+//18-42
+$("#18").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#18").hide();
+    $("#42").show();
+});
+
+//42-48
+$("#42").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#42").hide();
+    $("#48").show();
+});
+
+    //48-22
+$("#48").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#48").hide();
+    $("#22").show();
+});
+
+//22-37
+$("#22").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#22").hide();
+    $("#37").show();
+});
+
+//37-32
+$("#37").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#37").hide();
+    $("#32").show();
+});
+
+//32-07
+$("#32").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#32").hide();
+    $("#07").show();
+});
+
+//07-35
+$("#07").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#07").hide();
+    $("#35").show();
+});
+
+//35-28
+$("#35").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#35").hide();
+    $("#28").show();
+});
+
+//28-15
+$("#28").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#28").hide();
+    $("#15").show();
+});
+
+//15-36
+$("#15").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#15").hide();
+    $("#36").show();
+});
+
+//36-24
+$("#36").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#36").hide();
+    $("#24").show();
+});
+
+//24-17
+$("#24").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#24").hide();
+    $("#17").show();
+});
+
+//17-33
+$("#17").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#17").hide();
+    $("#33").show();
+});
+
+//33-16
+$("#33").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#33").hide();
+    $("#16").show();
+});
+
+//16-26
+$("#16").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#16").hide();
+    $("#26").show();
+});
+
+//fim de ao passar o mouse
+$("#26").click(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#26").hide();
+    $("#avancar").show();
+    $("#mensagens").show();
+});
+
+$("#avancar").click(function(){
+    $("#addCount").val('0');
+});
+
+
+});
---
+++ estilos/style - teclas.js
@@ -0,0 +1,568 @@
+$(document).ready(function(){
+    
+$("#avancar").hide();
+var count = 1;        
+
+$("#reiniciar").dblclick(function(){
+    window.location.reload(true);
+});
+
+
+//$("#01").hide();
+
+$("#mensagens").hide();
+$("#02").hide();
+$("#03").hide();
+$("#04").hide();
+$("#05").hide();
+$("#06").hide();
+$("#07").hide();
+$("#08").hide();
+$("#09").hide();
+$("#10").hide();
+$("#11").hide();
+$("#12").hide();
+$("#13").hide();
+$("#14").hide();
+$("#15").hide();
+$("#16").hide();
+$("#17").hide();
+$("#18").hide();
+$("#19").hide();
+$("#20").hide();
+$("#21").hide();
+$("#22").hide();
+$("#23").hide();
+$("#24").hide();
+$("#25").hide();
+$("#26").hide();
+$("#27").hide();
+$("#28").hide();
+$("#29").hide();
+$("#30").hide();
+$("#31").hide();
+$("#32").hide();
+$("#33").hide();
+$("#34").hide();
+$("#35").hide();
+$("#36").hide();
+$("#37").hide();
+$("#38").hide();
+$("#39").hide();
+$("#40").hide();
+$("#41").hide();
+$("#42").hide();
+$("#43").hide();
+$("#44").hide();
+$("#45").hide();
+$("#46").hide();
+$("#47").hide();
+$("#48").hide();
+$("#49").hide();
+$("#50").hide();
+$("#51").hide();
+$("#52").hide();
+$("#53").hide();
+$("#54").hide();
+$("#55").hide();
+$("#56").hide();
+$("#57").hide();
+$("#58").hide();
+$("#59").hide();
+$("#60").hide();
+
+//01-60
+$("#01").dblclick(function(){
+    $("#addCount").val(count + ' Ponto');
+    count++;
+    $("#60").show();
+    $("#01").hide();
+});
+
+//60-01-02
+$("#60").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#60").hide();
+    $("#02").show();
+});
+
+//02-59
+$("#02").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#02").hide();
+    $("#59").show();
+});
+
+//59-23
+$("#59").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#59").hide();
+    $("#23").show();
+});
+
+//23-10
+$("#23").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#23").hide();
+    $("#10").show();
+});
+
+//10-51
+ $("#10").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#10").hide();
+    $("#51").show();
+});
+
+
+//51-25
+ $("#51").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#51").hide();
+    $("#25").show();
+});
+
+//25-41
+$("#25").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#25").hide();
+    $("#41").show();
+});
+
+//41-30
+$("#41").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#41").hide();
+    $("#30").show();
+});
+
+
+//30-52
+$("#30").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#30").hide();
+    $("#52").show();
+});
+
+//52-08
+$("#52").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#52").hide();
+    $("#08").show();
+});
+
+ //08-11
+ $("#08").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#08").hide();
+    $("#11").show();
+});
+
+
+ //11-56
+$("#11").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#11").hide();
+    $("#56").show();
+});
+
+
+//56-21
+$("#56").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#56").hide();
+    $("#21").show();
+});
+
+
+ //21-54
+$("#21").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#21").hide();
+    $("#54").show();
+});
+
+
+ //54-20
+$("#54").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#54").hide();
+    $("#20").show();
+});
+
+ //20-03
+$("#20").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#20").hide();
+    $("#03").show();
+});
+
+
+ //03-55
+$("#03").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#03").hide();
+    $("#55").show();
+});
+
+ //55-09
+$("#55").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#55").hide();
+    $("#09").show();
+});
+
+ //09-57
+$("#09").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#09").hide();
+    $("#57").show();
+});
+
+ //57-12
+$("#57").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#57").hide();
+    $("#12").show();
+});
+
+ //12-58
+$("#12").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#12").hide();
+    $("#58").show();
+});
+
+ //58-05
+$("#58").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#58").hide();
+    $("#05").show();
+});
+
+ //05-39
+$("#05").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#05").hide();
+    $("#39").show();
+});
+
+ //39-34
+$("#39").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#39").hide();
+    $("#34").show();
+});
+
+ //34-40
+$("#34").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#34").hide();
+    $("#40").show();
+});
+
+ //40-06
+$("#40").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#40").hide();
+    $("#06").show();
+});
+
+
+ //06-46
+$("#06").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#06").hide();
+    $("#46").show();
+});
+
+//46-04
+$("#46").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#46").hide();
+    $("#04").show();
+});
+
+//04-50
+$("#04").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#04").hide();
+    $("#50").show();
+});
+
+//50-45
+$("#50").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#50").hide();
+    $("#45").show();
+});
+
+//45-13
+$("#45").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#45").hide();
+    $("#13").show();
+});
+
+//13-49
+$("#13").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#13").hide();
+    $("#49").show();
+});
+
+//49-43
+$("#49").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#49").hide();
+    $("#43").show();
+});
+
+//49-27
+$("#43").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#43").hide();
+    $("#27").show();
+});
+
+//27-53
+$("#27").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#27").hide();
+    $("#53").show();
+});
+
+//53-38
+$("#53").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#53").hide();
+    $("#38").show();
+});
+
+//38-14
+$("#38").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#38").hide();
+    $("#14").show();
+});
+
+//14-19
+$("#14").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#14").hide();
+    $("#19").show();
+});
+
+//19-44
+$("#19").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#19").hide();
+    $("#44").show();
+});
+
+//44-29
+$("#44").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#44").hide();
+    $("#29").show();
+});
+
+//29-47
+$("#29").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#29").hide();
+    $("#47").show();
+});
+
+//47-31
+$("#47").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#47").hide();
+    $("#31").show();
+});
+
+//31-18
+$("#31").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#31").hide();
+    $("#18").show();
+});
+
+//18-42
+$("#18").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#18").hide();
+    $("#42").show();
+});
+
+//42-48
+$("#42").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#42").hide();
+    $("#48").show();
+});
+
+    //48-22
+$("#48").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#48").hide();
+    $("#22").show();
+});
+
+//22-37
+$("#22").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#22").hide();
+    $("#37").show();
+});
+
+//37-32
+$("#37").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#37").hide();
+    $("#32").show();
+});
+
+//32-07
+$("#32").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#32").hide();
+    $("#07").show();
+});
+
+//07-35
+$("#07").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#07").hide();
+    $("#35").show();
+});
+
+//35-28
+$("#35").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#35").hide();
+    $("#28").show();
+});
+
+//28-15
+$("#28").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#28").hide();
+    $("#15").show();
+});
+
+//15-36
+$("#15").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#15").hide();
+    $("#36").show();
+});
+
+//36-24
+$("#36").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#36").hide();
+    $("#24").show();
+});
+
+//24-17
+$("#24").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#24").hide();
+    $("#17").show();
+});
+
+//17-33
+$("#17").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#17").hide();
+    $("#33").show();
+});
+
+//33-16
+$("#33").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#33").hide();
+    $("#16").show();
+});
+
+//16-26
+$("#16").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#16").hide();
+    $("#26").show();
+});
+
+//fim de ao passar o mouse
+$("#26").dblclick(function(){
+    $("#addCount").val(count + ' Pontos');
+    count++;
+    $("#26").hide();
+    $("#avancar").show();
+    $("#mensagens").show();
+});
+
+$("#avancar").click(function(){
+    $("#addCount").val('0');
+});
+
+
+});
